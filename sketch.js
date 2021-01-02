5de7f0fffa0e392b83a14f2329469b176e5e24b1
Abhiraksh <abhirakshkesarwani@gmail.com>
2021-01-02 12:22:13 +0800

    Files added via Git Bash

--- sketch.js
+++ sketch.js
@@ -9,14 +9,26 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car_img1, car_img2, car_img3, car_img4, track_img;
 
+function preload(){
+ 
+  car_img1 = loadImage("images/car1.png");
+  car_img2 = loadImage("images/car2.png");
+  car_img3 = loadImage("images/car3.png");
+  car_img4 = loadImage("images/car4.png");
+  track_img = loadImage("images/track.png");
+
+}
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  canvas = createCanvas(displayWidth+120, displayHeight-60);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
+
+
 }
 
 
@@ -27,5 +39,9 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+
+  if(gameState === 2){
+    game.end();
   }
 }
